1e347315aa1e839c742c3bd85532e9e48b634524
MatheusViols <matheusviolss@gmail.com>
2026-04-05 21:44:26 -0300

    feat: senhas-service

---
+++ README.md
@@ -0,0 +1,22 @@
+## Mobile Tickets Ionic
+Este projeto foi desenvolvido para avaliação da primeira unidade na disciplina de Desenvolvimento Para Dispositívos móveis e trata-se de um aplicativo para controle de atendimento em filas de um laboratório médico.
+
+#### Funcionalidades
+O app é capaz de criar novas senhas e apresentar o número de senhas totais criadas para cada grupo de senhas. Sendo eles:
+
+- SG - Senha Geral
+- SP - Senha Prioritária
+- SE - Senha Exame
+
+#### Ferramentas utilizadas
+- Ionic 7.2.1
+- Angular 20.3.22
+- Node 22.22.2
+- TypeScript 5.9.3
+
+#### Como Utilizar
+- `npm install` para instalar dependencias
+- `ionic serve` para rodar a aplicação
+
+
+
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
+import { SenhasService } from '../services/senhas';
+
+import { IonCard, IonCardContent, IonList, IonLabel, IonItem } from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    ExploreContainerComponent,
+    IonCard, IonCardContent, IonList, IonLabel, IonItem,
+  ],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor(public senhasService: SenhasService) {}
 }
